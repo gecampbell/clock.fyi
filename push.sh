8d89677bed12be8447783998856f6f1280fac995
Glen Campbell <glen@broadpool.com>
2015-10-01 13:21:09 -0700

    syncing

---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -d site ]; then
+  cd site
+  echo "Synchronizing..."
+  aws s3 sync . s3://clock.fyi --cache-control 'max-age=300' --delete
+else
+  echo "Can't find the site/ directory, exiting..."
+  exit
+fi
+
+echo Done
