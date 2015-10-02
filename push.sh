3c637a6030835639ea8455507c6a66faf46a165f
Glen Campbell <glen@broadpool.com>
2015-10-02 11:26:36 -0700

    reformatted

--- push.sh
+++ push.sh
@@ -3,7 +3,9 @@
 if [ -d site ]; then
   cd site
   echo "Synchronizing..."
-  aws s3 sync . s3://clock.fyi --cache-control 'max-age=300' --delete
+  aws s3 sync . s3://clock.fyi \
+    --cache-control 'max-age=300' \
+    --delete
 else
   echo "Can't find the site/ directory, exiting..."
   exit
